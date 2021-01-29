e31f00e419b2f0ab4cf124f2731f4952c3e95e95
zenraiching <rickdavidsangalang@gmail.com>
2021-01-29 22:35:46 +0800

    try

--- dashboard.php
+++ dashboard.php
@@ -2,12 +2,25 @@
 
 $username = $_POST['username'];
 $password = $_POST['password'];
-
+$uname = "admin";
+$pwd = "admin";
 session_start();
 if(isset($_SESSION['username'])){
-    echo "Yehey";
+    echo "<h1>Yehey".$_SESSION["username"]."</h1>";
+    echo "<br> <a href='lougout.php'> <input type='button' value=logout name=logout></a>";
+
 }
 else{
+    if($username == $uname && $password == $pwd){
+        $_SESSION['username'] = $uname;
+        echo "<script> location.href = 'dashboard.php' </script>";
+    }
+    else{
+        echo "<script> alert('ONE MORE') </script>";
+        echo "<script> location.href ='login.php' </script>";
+    
+    }
+    
     
 }
 
