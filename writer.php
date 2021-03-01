5192436f932356173e67815282e9bff875380931
zenraiching <rickdavidsangalang@gmail.com>
2021-03-01 23:39:52 +0800

    add stripping remover

--- writer.php
+++ writer.php
@@ -16,9 +16,17 @@ session_start();
     require_once 'Parsedown.php';
     $Parsedown = new Parsedown();
 
-    echo $Parsedown->text('# Hello _Parsedown_!<br> fdd');
-    echo $Parsedown->parse('# Hello _Parsedown_!<br> fdd');
-    echo $Parsedown->line('# Hello _Parsedown_!<br> fdd');
+    $parsed =  $Parsedown->text('# Hello _Parsedown_! fdd');
+    echo strip_tags($parsed);
+    echo $Parsedown->parse('# Hello _Parsedown_! <br> fdd');
+
+    
+    echo $Parsedown->line('# Hello _Parsedown_!<br> ');
+    $class_methods = get_class_methods(new Parsedown());
+
+    foreach ($class_methods as $method_name) {
+        echo "$method_name\n";
+    }
 
     ?>
 <?php
