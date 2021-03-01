dc269d3b8cda8744e12e994db9dbbb75aad9b417
zenraiching <rickdavidsangalang@gmail.com>
2021-03-02 01:12:44 +0800

    can Live preview

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # blogapp-php
 
 ## RESOURCES:
+
+**WRITE A POST**
 https://www.studentstutorial.com/ajax/insert-data
 
 
 https://www.youtube.com/watch?v=gvGb5Z0yMFY&ab_channel=Satish
+
+**FORGET PASSWORD**
+https://www.allphptricks.com/forgot-password-recovery-reset-using-php-and-mysql/
+
--- writer.php
+++ writer.php
@@ -1,30 +1,52 @@
 <?php
-error_reporting(0);
-
-session_start();
+    error_reporting(0);
+    session_start();
 ?>
 <html>
+
 <head>
-<title>User Login</title>
+    <title>User Login</title>
 </head>
+
 <body>
     <div>
         <h1>Write Blog </h1>
     </div>
-<?php
-    if($_SESSION["name"]) {
-    ?>
+    
+<textarea onkeyup="preview()" type="text" id="TextArea" ></textarea>
+    <div id="content"></div>
+    <button onclick="testPHP()" id="testButton">testing</button>
+    <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script>
+        
+        function preview(){
+            $("#content").html(
+                marked($("#TextArea").val())
+                
+                );
+        }
+
+        function testPHP(){
+            
+        }
+        
+        
+    </script>
+
     <?php
-    
-    
+        if($_SESSION["name"]) {
+    ?><?php
 
-    ?>
-<?php
+
+
+
+    ?><?php
     }
-
-    else {
+        else {
         header("Location:login.php");
-    };
-?>
+        };
+    ?>
 </body>
+
 </html>
