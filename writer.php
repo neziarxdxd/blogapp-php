cfa6453b1c7fa71c0bb1ff0933cfaabdb54af118
zenraiching <rickdavidsangalang@gmail.com>
2021-03-02 19:15:13 +0800

    chane

--- writer.php
+++ writer.php
@@ -18,22 +18,18 @@
     <button onclick="testPHP()" id="testButton">testing</button>
     <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script>
-        
-        function preview(){
+    <script>        
+        function preview() {
             $("#content").html(
                 marked($("#TextArea").val())
-                
-                );
-        }
-
-       
-        
+            );
+        }        
         
     </script>
 
     <?php
         if($_SESSION["name"]) {
+            
     ?><?php
 
 
