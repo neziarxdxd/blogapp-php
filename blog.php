421d087a493fd5ff76399f2e66e29449f8150b29
zenraiching <rickdavidsangalang@gmail.com>
2021-03-04 00:37:43 +0800

    move files

---
+++ blog.php
@@ -0,0 +1,53 @@
+<?php
+error_reporting(0);
+
+session_start();
+?>
+<html>
+<head>
+<title>User Login</title>
+<style>
+    body{
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
+</head>
+<body>
+
+<?php
+    if($_SESSION["name"]) {
+    ?>
+    <?php
+    
+    require_once 'modules/Parsedown.php';
+    $Parsedown = new Parsedown();
+    $full_content = "";
+    $con = mysqli_connect('127.0.0.1:3306','root','','blog_database') or die('Unable To connect');
+    $sql ="SELECT blog_story FROM `blog_user` WHERE blog_id=1007";
+    $result = mysqli_query($con,$sql);
+    if(mysqli_num_rows($result) > 0)  {
+        while($row = mysqli_fetch_array($result)){                                  //return true;  
+            $full_content = $row['blog_story'];
+        }       
+    }
+    else{
+    echo "error";
+    }
+    
+   
+    $parsed =  $Parsedown->text('# Hello _Parsedown_! fdd');   
+    echo $Parsedown->parse($full_content);
+
+    
+    
+
+    ?>
+<?php
+    }
+
+    else {
+        header("Location:login.php");
+    };
+?>
+</body>
+</html>
