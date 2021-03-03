1b836ae43f1481c92a4c9387e96132bfaca37e38
zenraiching <rickdavidsangalang@gmail.com>
2021-03-03 21:06:00 +0800

    add

--- writer.php
+++ writer.php
@@ -17,10 +17,12 @@
     <div>
         <h1>Write Blog <?php echo $_SESSION["name"]; ?></h1>
     </div>
-    
-<textarea onkeyup="preview()" type="text" id="TextArea" ></textarea>
+    <form name="form_blog" method="post" action="" >
+    <textarea onkeyup="preview()" type="text" id="TextArea" name="blog_story" ></textarea>
+    <input type="submit" name="submit" value="Submit">
+    </form>
     <div id="content"></div>
-    <button onclick="testPHP()" id="testButton">testing</button>
+   
     <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script>        
@@ -34,6 +36,22 @@
 
     <?php
         if($_SESSION["name"]) {
+            if(count($_POST)>0) {
+                $con = mysqli_connect('127.0.0.1:3306','root','','blog_database') or die('Unable To connect');
+        
+                $blog_story = $_POST["blog_story"];
+                $user_id = $_SESSION["id"];
+              
+                $sql = "INSERT INTO `blog_user` (`user_name`,`blog_story`) VALUES ('$user_id','$blog_story');";
+        
+                if ($con->query($sql) === TRUE) {
+                echo "New record created successfully";
+                } else {
+                echo "Error: " . $sql . "<br>" . $con->error;
+                }
+        
+                $con->close();
+            }
 
     ?><?php
 
