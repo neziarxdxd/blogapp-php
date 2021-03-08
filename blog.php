f0f4e52cb700c43d9f2e39c0b46730e3efe32c80
zenraiching <rickdavidsangalang@gmail.com>
2021-03-08 11:32:15 +0800

    add

--- blog.php
+++ blog.php
@@ -22,8 +22,10 @@ session_start();
     require_once 'modules/Parsedown.php';
     $Parsedown = new Parsedown();
     $full_content = "";
+    $story_id = $_GET['blogstory'];
+    
     $con = mysqli_connect('127.0.0.1:3306','root','','blog_database') or die('Unable To connect');
-    $sql ="SELECT blog_story FROM `blog_user` WHERE blog_id=1007";
+    $sql ="SELECT blog_story FROM `blog_user` WHERE blog_id=$story_id";
     $result = mysqli_query($con,$sql);
     if(mysqli_num_rows($result) > 0)  {
         while($row = mysqli_fetch_array($result)){                                  //return true;  
--- index.php
+++ index.php
@@ -17,6 +17,17 @@ session_start();
         <a href="writer.php">Write post</a>
         <br>Click here to <a href="logout.php" tite="Logout">Logout.
 <?php
+$con = mysqli_connect('127.0.0.1:3306','root','','blog_database') or die('Unable To connect');
+$sql ="SELECT blog_story FROM `blog_user` WHERE blog_id=$story_id";
+$result = mysqli_query($con,$sql);
+if(mysqli_num_rows($result) > 0)  {
+    while($row = mysqli_fetch_array($result)){                                  //return true;  
+       echo $row['blog_story'];
+    }       
+}
+else{
+echo "error";
+}
     }
 
     else {
