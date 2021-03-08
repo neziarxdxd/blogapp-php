84c2ee83f36a5e4464b593a086a72a3060e2ea0c
zenraiching <rickdavidsangalang@gmail.com>
2021-03-08 15:01:30 +0800

    add

--- index.php
+++ index.php
@@ -11,14 +11,33 @@ session_start();
 
 <?php
     if($_SESSION["name"]) {
-
-        
+    
+    
     ?>
         Welcome <?php echo $_SESSION["name"]; ?>.
         <br>
-        <a href="writer.php">Write post</a>
-        <br>Click here to <a href="logout.php" tite="Logout">Logout.
+        <?php
+    $con = mysqli_connect('127.0.0.1:3306','root','','blog_database') or die('Unable To connect');
+    $result = mysqli_query($con,"SELECT * FROM blog_user WHERE user_name='" . $_SESSION["id"]."'");
+    $row = mysqli_fetch_array($result);
+  
+    
+   
+    while($row = mysqli_fetch_array($result)){
+         
+                  //return true;  
+                echo "<p><a href='blog.php?blogstory=".$row['blog_id']."'>Data:".$row['blog_id']."</a></p>";
+                 
+              
+    }
 
+
+
+    
+    ?>
+        <a href="writer.php">Write post</a>
+        <br>Click here to <a href="logout.php" tite="Logout">Logout.</a>
+    
         
 <?php
 
