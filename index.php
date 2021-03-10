82791fab7b1f6fec9f4d792023fa013386dad034
zenraiching <rickdavidsangalang@gmail.com>
2021-03-10 16:23:48 +0800

    update to delete

--- index.php
+++ index.php
@@ -18,8 +18,10 @@ session_start();
     $con = mysqli_connect('127.0.0.1:3306','root','','blog_database') or die('Unable To connect');
     $result = mysqli_query($con,"SELECT * FROM blog_user WHERE user_name='" . $_SESSION["id"]."'");
     $row = mysqli_fetch_array($result);  
-   
-    while($row = mysqli_fetch_array($result)){
+   echo $row[1];
+
+   // i'll delte this try to use for loop
+    while($row = mysqli_fetch_array($result)){        
         echo "<p><a href='blog.php?blogstory=".$row['blog_id']."'>Data:".$row['blog_id']."</a> 
         <a href='rewrite.php?edit-blog=".$row['blog_id']."'> edit</a> </p>";
                     
