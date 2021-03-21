9f7edf2733aab62266ff7fe899987838fdc6e281
zenraiching <rickdavidsangalang@gmail.com>
2021-03-21 17:45:15 +0800

    add

--- writer.php
+++ writer.php
@@ -9,12 +9,18 @@
     <title>New Blog Post Template | PrepBootstrap</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
 
-    <link rel="stylesheet" type="text/css" href="font-awesome-2/css/font-awesome.min.css" />
-    
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <!-- <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'> -->
+  <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
     <script type="text/javascript" src="js-2/jquery-1.10.2.min.js"></script>
-    <script type="text/javascript" src="bootstrap-2/js/bootstrap.min.js"></script>
+  
+    
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 
     <!-- ADD -->
@@ -24,26 +30,41 @@
 <body>
 
 
-<?php include 'navbar.php'?>
-<header></header>
+<nav class="navbar navbar-expand-lg navbar-light bg-light right ">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav mr-auto">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Features</a>
+      <a class="nav-item nav-link" href="#">Pricing</a>
+      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+    </div>
+  </div>
+</nav>
 <!-- New Blog Post - START -->
 <div class="container">
     <div class="row" id="row_style">
         <h4 class="text-center">Submit new post</h4>
         <div class="col-md-12   col-md-offset-12">
         <form method="POST" name="submit">
-            <div class="form-group">
-                <input type="text" name="blog_title" class="form-control" placeholder="Title">
-            </div>
-            <textarea id="editor" name="blog_story" cols="30" rows="10"></textarea>
+                <div class="form-group">
+                    <input type="text" name="blog_title" class="form-control" placeholder="Title">
+                </div>
+                    <textarea id="editor" name="blog_story" cols="30" rows="10"></textarea>
             <br>
             
-            <div class="form-group">
-                <input onclick="testing()" type="submit" name="submit" class="btn btn-primary" id="submit"></button>
-                </form>
-            </div>
+                    <div class="form-group">
+                        <input onclick="testing()" type="submit" name="submit" class="btn btn-primary" id="submit"></button>
+                    
+                    </div>
+        
+        </form>
         </div>
     </div>
+  
 </div>
 
 <style>
