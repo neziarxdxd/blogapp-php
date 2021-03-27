c75f564014696f232d5b42fcfce043e28dd7ce8b
zenraiching <rickdavidsangalang@gmail.com>
2021-03-27 09:51:55 +0800

    add

--- delete.php
+++ delete.php
@@ -3,27 +3,23 @@
 session_start();
         if($_SESSION["name"]) {
             ?>
-            <?php     
-           
-            
+            <?php                         
             
             
             include 'connect.php';
-            $user_name = $_SESSION['id'];
-            
-            
-               
-                $user_id = $_SESSION["id"];
-        
+                
+             
+                $sql = "DELETE FROM `blog_user` WHERE blog_id=? and user_name=? ";
+
+              
+                $user_id = $_SESSION["id"];        
                 $blog_id = $_GET['delete_id'];
 
+                $statement = $con->prepare($sql); 
+                $statement->bind_param("ss",$blog_id,$user_id);            
+                        
                 
-                
-                $insert_data = mysqli_real_escape_string($con, $blog_story);
-                // inserting data
-                $sql = "DELETE FROM `blog_user` WHERE blog_id='$blog_id' and user_name='$user_id'";
-                
-                if ($con->query($sql) === TRUE) {                    
+                if ($statement->execute()){                    
                     header("location:user_profile.php");
                     exit;                    
                 } 
